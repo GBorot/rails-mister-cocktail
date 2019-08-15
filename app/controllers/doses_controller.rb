7f2cedd2ae685fe328199573745d756fe0c6f7bf
Gautier Borot <g.borot@gmail.com>
2019-08-15 17:07:08 +0200

    controllers doses & cocktails with views working

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -9,14 +9,16 @@ class DosesController < ApplicationController
   end
 
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
-
+    @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to dose_path(@dose), notice: 'Dose was successfully created.'
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
@@ -31,14 +33,15 @@ class DosesController < ApplicationController
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to doses_path
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 
   def set_dose
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+
+Dose.destroy_all
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+lemon = Ingredient.create!(name: "lemon")
+ice = Ingredient.create!(name: "ice")
+mint = Ingredient.create!(name: "mint leaves")
+
+azerty = Cocktail.create!(name: "azerty")
+hummy = Cocktail.create!(name: "Hummy")
+gizzzz = Cocktail.create!(name: "Gizzzz")
+humtiuatan = Cocktail.create!(name: "Humtiuatan")
+
+Dose.create!(description: "5cl", ingredient: ice, cocktail: azerty)
+Dose.create!(description: "15cl", ingredient: lemon, cocktail: gizzzz)
+Dose.create!(description: "20cl", ingredient: mint, cocktail: humtiuatan)
+
